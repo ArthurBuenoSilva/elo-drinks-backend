cf1915465224abb07be370e3ba43357d548c2336
Arthur Bueno Silva <arthurbueno1395271@hotmail.com>
2025-03-30 10:21:04 -0300

    Fix lint in 2 files

--- code/elo_drinks/wsgi.py
+++ code/elo_drinks/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'elo_drinks.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "elo_drinks.settings")
 
 application = get_wsgi_application()
