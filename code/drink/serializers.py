093efe9d20cde7b6963d0aced9cc6e11541a2b70
LucasObana <lucas.sawada@ges.inatel.br>
2025-04-17 18:27:23 -0300

    Update for lint process

--- code/drink/serializers.py
+++ code/drink/serializers.py
@@ -1,18 +1,31 @@
 from rest_framework import serializers
+
 from .models import Category, Drink, OrderDrink
+
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = ["id", "name"]
+
 
 class DrinkSerializer(serializers.ModelSerializer):
     class Meta:
         model = Drink
-        fields = ['id', 'name', 'description', 'ingredients', 'is_open_letter', 'price', 'available', 'category', 'created_at']
+        fields = [
+            "id",
+            "name",
+            "description",
+            "ingredients",
+            "is_open_letter",
+            "price",
+            "available",
+            "category",
+            "created_at",
+        ]
+
 
 class OrderDrinkSerializer(serializers.ModelSerializer):
- 
     class Meta:
         model = OrderDrink
-        fields = ['id', 'order_id', 'drink', 'quantity', 'total_price']
+        fields = ["id", "order_id", "drink", "quantity", "total_price"]
--- code/order/serializers.py
+++ code/order/serializers.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
+
 from .models import Order, OrderStatus
 
 
 class OrderStatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderStatus
-        fields = ['id', 'status']
+        fields = ["id", "status"]
 
 
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ['id', 'user', 'event', 'total_price', 'status', 'establishment_fee', 'payment_status', 'created_at']
+        fields = ["id", "user", "event", "total_price", "status", "establishment_fee", "payment_status", "created_at"]
